b859b8fda8ba17a5f78d0abe37e731d86b208b0d
Ralph Bloch <ralph.bloch@bell.net>
2023-03-30 17:40:58 -0400

    edit Help_0

--- src/main/resources/org/gsusers/gsmv/utilities/help/Help_0.tf
+++ src/main/resources/org/gsusers/gsmv/utilities/help/Help_0.tf
@@ -4,3 +4,4 @@
 <sub>File > Start over|Reset and start over</sub>
 <sub>File > Save Results|At the end of an analysis, to save all the intermediate and final results in a text file</sub>
 <sub>File > Exit|You can exit the program any time you wish.</sub>
+<sub>Action > Analyze new|If you have to start analyzing a new problem from scratch, this is where you start. The program will then guide you step by step through the whole process. It leads you to the desired G - Study results and the chance to do a D - Study.</sub>

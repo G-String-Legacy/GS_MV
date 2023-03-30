46d5ae3c7cf33f5dd15b32f84a1f8a50d0cf4db9
Ralph Bloch <ralph.bloch@bell.net>
2023-03-30 17:42:10 -0400

    edit Help_0

--- src/main/resources/org/gsusers/gsmv/utilities/help/Help_0.tf
+++ src/main/resources/org/gsusers/gsmv/utilities/help/Help_0.tf
@@ -5,3 +5,6 @@
 <sub>File > Save Results|At the end of an analysis, to save all the intermediate and final results in a text file</sub>
 <sub>File > Exit|You can exit the program any time you wish.</sub>
 <sub>Action > Analyze new|If you have to start analyzing a new problem from scratch, this is where you start. The program will then guide you step by step through the whole process. It leads you to the desired G - Study results and the chance to do a D - Study.</sub>
+<sub>Action > Re-analyze|If you already have a script with the necessary information to re-run a previous analysis.</sub>
+<sub>Action > Analyze Replication|To run an analysis of a new replicating data set.</sub>
+<sub>Action > Re-analyze Replication|To re-analyze a replicating data set with an exiating script</sub>

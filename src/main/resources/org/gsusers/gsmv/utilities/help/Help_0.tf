d11fbd3dcc552b049a78bbd06936b4a2e81cb74b
Ralph Bloch <ralph.bloch@bell.net>
2023-03-30 17:37:25 -0400

    edit Help_0

--- src/main/resources/org/gsusers/gsmv/utilities/help/Help_0.tf
+++ src/main/resources/org/gsusers/gsmv/utilities/help/Help_0.tf
@@ -1,2 +1,3 @@
 <title>Starting an analysis</title>
 <para>This is the start up screen. You find your choices in the menu bar at the top of this screen. Here are some of the things, you can do:</para>
+<sub>File > Start over|Reset and start over</sub>

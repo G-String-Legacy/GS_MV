40dbd1e6bb67e505021361ad5aff3b4d05850f2e
Ralph Bloch <ralph.bloch@bell.net>
2023-03-30 17:39:34 -0400

    edit Help_0

--- src/main/resources/org/gsusers/gsmv/utilities/help/Help_0.tf
+++ src/main/resources/org/gsusers/gsmv/utilities/help/Help_0.tf
@@ -1,3 +1,6 @@
 <title>Starting an analysis</title>
 <para>This is the start up screen. You find your choices in the menu bar at the top of this screen. Here are some of the things, you can do:</para>
 <sub>File > Start over|Reset and start over</sub>
+<sub>File > Start over|Reset and start over</sub>
+<sub>File > Save Results|At the end of an analysis, to save all the intermediate and final results in a text file</sub>
+<sub>File > Exit|You can exit the program any time you wish.</sub>

5bb12b8dba0cb51d2f9bc11ec7decac5faa2c816
Ralph Bloch <ralph.bloch@bell.net>
2023-03-30 17:36:14 -0400

    edit Help_0

--- src/main/resources/org/gsusers/gsmv/utilities/help/Help_0.tf
+++ src/main/resources/org/gsusers/gsmv/utilities/help/Help_0.tf
@@ -1,1 +1,2 @@
 <title>Starting an analysis</title>
+<para>This is the start up screen. You find your choices in the menu bar at the top of this screen. Here are some of the things, you can do:</para>
